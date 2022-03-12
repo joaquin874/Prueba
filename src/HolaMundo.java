0025db83d8eb6ce721b226bc88cb7bc30abfc928
joaquin <joaquin.pary@mi.unc.edu.ar>
2022-03-11 23:29:47 -0300

    nueva version de java

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
 public class HolaMundo {
-
+String valoradar;
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
+        String valoraux;
+        Scanner valorEntrada = new Scanner(System.in);
+        valoraux = valorEntrada.nextLine();
+        visordemensaje(valoraux);
     }
 
+    private static void visordemensaje(String valor) {
+        System.out.println("Ingrese un valor");
+    }
 }
